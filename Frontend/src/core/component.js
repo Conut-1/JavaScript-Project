ff0207337da97c565679c9147a5f6830de5e2c42
CH_Kim <1mim1@naver.com>
2022-09-02 20:44:24 +0900

    chore: component core 오타 수정

--- Frontend/src/core/component.js
+++ Frontend/src/core/component.js
@@ -30,7 +30,7 @@ export default class Component {
 
     addEvent(eventType, selector, callback) {
         const children = [...this.target.querySelectorAll(selector)];
-        const isTarget = (target) => children.includes(target) || target.cloest(selector);
+        const isTarget = (target) => children.includes(target) || target.closest(selector);
         this.target.addEventListener(eventType, (event) => {
             if (!isTarget(event.target)) return false;
             callback(event);
